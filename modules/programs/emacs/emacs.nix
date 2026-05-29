bcd1f523aff4b492c75b3f7ed6a244ec95b57545
Zach Bedewi <zachary.bedewi@protonmail.com>
2026-05-29 14:49:27 -0500

    Updating path

--- modules/programs/emacs/emacs.nix
+++ modules/programs/emacs/emacs.nix
@@ -130,7 +130,7 @@
               fd
             ]
           )
-        }:/opt/homebrew/bin:$PATH"
+        }:/opt/homebrew/bin:/usr/bin:$PATH"
         EMACSDIR="$HOME/.config/emacs"
         DOOMBIN="$EMACSDIR/bin/doom"
 
