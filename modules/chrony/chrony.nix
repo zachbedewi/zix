331e4b6d63df62475cb417aa3dc3117e0c9d4d4f
Zach Bedewi <zachary.bedewi@protonmail.com>
2026-08-29 22:41:25 +0000

    Fixing chrony setup

--- modules/chrony/chrony.nix
+++ modules/chrony/chrony.nix
@@ -5,6 +5,11 @@
       enableRTCTrimming = true;
       enableNTS = true;
       serverOption = "iburst";
+      servers = [
+        "time.cloudflare.com"
+        "nts.netnod.se"
+        "ptbtime1.ptb.de"
+      ];
 
       extraConfig = ''
         makestep 1.0 3
